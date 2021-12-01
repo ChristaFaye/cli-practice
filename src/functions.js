4065d54f72cf83d26146938d1369f6442d3ff94a
christafayereyes <christafayereyes@gmail.com>
2021-11-30 23:20:50 -0600

    Moved currency check functions to functions.js

--- src/functions.js
+++ src/functions.js
@@ -1,3 +1,4 @@
+//Functions for user input validation
 const isAmountUndefined = (amount) => {
     return amount === undefined || amount < 0;
 };
@@ -10,8 +11,22 @@ const isTargetCurrenctyUndefined = (targetCurrency) => {
     return targetCurrency === undefined;
 };
 
+
+//Functions for checking if currency is supported
+const isIntCurrencyIncluded = (intCurrency) => {
+    return supportedCurrencies.includes(intCurrency) === false;
+};
+
+const isTargetCurrencyIncluded = (targetCurrency) => {
+    return supportedCurrencies.includes(targetCurrency) === false;
+};
+
 module.exports = {
+    //User Input Validation functions
     isAmountUndefined,
     isIntCurrencyUndefined,
-    isTargetCurrenctyUndefined
+    isTargetCurrenctyUndefined,
+    //Supported currencies check functions
+    isIntCurrencyIncluded,
+    isTargetCurrencyIncluded
 } ;
